0217410e1dfd9f64b23788f18c6cde8f61ccf423
Arthur Saguez <arthur.saguez@gmail.com>
2022-12-05 15:13:58 +0100

    v3

--- 04_openfile.py
+++ 04_openfile.py
@@ -1,4 +1,13 @@
 #Ouvrir un fichier avec python
-with open ("text.txt", "a") as f:
-    #append ajoute alors que write remplace
-    f.write("Hello World")
+# with open ("text.txt", "a") as f:#as file
+#     #append ajoute alors que write remplace
+#     f.write("Hello World")
+
+import sys
+
+fichier = input("Indiquer le fichier à éditer :")
+contenu = input("Quel texte ajoutez-vous ?")
+
+fichier, contenu = sys.argv[1], sys.argv[2]
+with open(fichier, "w") as f:
+    f.write(contenu)
---
+++ 05_openfile3args.py
@@ -0,0 +1,9 @@
+import sys
+
+fichier, contenu, fin, nbr = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+contenu = contenu + "\n"
+with open(fichier, "w") as f:
+    f.write(contenu * int(nbr))
+
+with open(fichier, "a") as f:
+    f.write(fin)
