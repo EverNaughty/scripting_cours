b068c994b854137753d88748dbad21a5f956071a
Arthur Saguez <arthur.saguez@gmail.com>
2022-12-05 15:18:57 +0100

    v4

--- 05_openfile4args.py
+++ 05_openfile4args.py
@@ -4,6 +4,4 @@ fichier, contenu, fin, nbr = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 contenu = contenu + "\n"
 with open(fichier, "w") as f:
     f.write(contenu * int(nbr))
-
-with open(fichier, "a") as f:
     f.write(fin)
