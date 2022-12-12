467302b3e975f5f6542136ec77fb90f032e26338
Arthur Saguez <arthur.saguez@gmail.com>
2022-12-12 08:43:02 +0100

    v1

---
+++ texte01.py
@@ -0,0 +1,15 @@
+# Twinkle, twinkle, little star,
+#     How I wonder what you are!
+#         Up above the world so high,
+#         Like a diamond in the sky.
+# Twinkle, twinkle, little star,
+#     How I wonder what you are
+
+l1 = "Twinkle, twinkle, little star,\n"
+l2 = "  How I wonder what you are!\n"
+l3 = "      Up above the world so high,\n"
+l4 = "      Like a diamond in the sky.\n"
+l5 = "Twinkle, twinkle, little star,\n"
+l6 = "  How I wonder what you are"
+
+print (l1,l2,l3,l4,l5,l6)
