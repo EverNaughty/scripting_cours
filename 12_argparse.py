11f3770f97f44b01d5151d33a63cc14dcf7e2db3
Arthur Saguez <arthur.saguez@gmail.com>
2022-12-12 11:37:53 +0100

    v2

--- 12_argparse.py
+++ 12_argparse.py
@@ -7,21 +7,28 @@
 # Renvoi des instructions d'utilisation en cas d'erreur lors de l'appel du script, génère un manuel
 import argparse
 
+# Débit du parser
 parser = argparse.ArgumentParser(
                     prog = "Gatling a Emojis V1",
                     description = "Floodeur d'emojis pro",
                                 )
 
-
+# Listing des arguments attendus
 parser.add_argument('-e', '--emoji')
+# Valeur par défaut si argument non utilisé
 parser.add_argument('-n', '--number', default=5)
 
 args = parser.parse_args()
+
+# On stocke les arguments dans des variables
 emoji = args.emoji
 nombre = args.number
+# Transformation de la variable STR en INT
 nombre = int(nombre)
+# Multiplication
 result = emoji*nombre
 
+# Pas d'opération au dessus de 15
 if nombre >= 16:
     print ("Invalide")
 else:
